32324594fff07ad82002e55f450905d453d16496
aost <3398@winnermedical.com>
2021-07-14 15:00:44 +0800

    Initial commit

---
+++ src/main/java/com/example/rabbitmq_test/RabbitmqTestApplication.java
@@ -0,0 +1,16 @@
+package com.example.rabbitmq_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author aost
+ */
+@SpringBootApplication
+public class RabbitmqTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqTestApplication.class, args);
+    }
+
+}
